e2d9a5ef014301fe0598736e8bcf9fd7f8d76f62
staman1702 <staman1702@gmail.com>
2024-04-18 07:22:17 +0100

    dependency resolution

--- go_fit/settings.py
+++ go_fit/settings.py
@@ -51,7 +51,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'go_fit.urls'
