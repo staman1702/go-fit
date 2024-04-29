dec1ed9daac81bdfb420ebdd210d8513a0708656
staman1702 <staman1702@gmail.com>
2024-04-29 23:09:35 +0100

    Updated community all_posts view (and url/html)

--- community/urls.py
+++ community/urls.py
@@ -2,6 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='community'),
+    path('', views.all_posts, name='community'),
     path('<slug:slug>/', views.post_detail, name='post_detail'),
 ]
--- community/views.py
+++ community/views.py
@@ -1,14 +1,28 @@
 from django.shortcuts import render, get_object_or_404
-from django.views import generic
-from .models import Post
+
+from .models import Post, Subject
 
 # Create your views here.
 
-class PostList(generic.ListView):
-    """ A view to show all post """
-    queryset = Post.objects.filter(status=1)
-    template_name = "community/community.html"
-    paginate_by = 6
+def all_posts(request):
+    
+    posts = Post.objects.filter(status=1)
+    subjects = None    
+
+    if request.GET:        
+        if 'subject' in request.GET:
+            subjects = request.GET['subject'].split(',')
+            posts = posts.filter(subject__name__in=subjects)
+            subjects = Subject.objects.filter(name__in=subjects)
+
+
+
+    context = {
+        'posts': posts,
+        'current_subjects': subjects,
+    }
+
+    return render(request, 'community/community.html', context)
 
 def post_detail(request, slug):
     
